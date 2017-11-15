262b54dd5ff1c8857e764857af264c3f39718d10
Karolina <karolina.rostek.93@gmail.com>
2017-11-15 21:34:45 +0100

    TitleBracketsValidator class for task 4

--- pairguru-master/app/validator/title_brackets_validator.rb
+++ pairguru-master/app/validator/title_brackets_validator.rb
@@ -1,18 +1,33 @@
 class TitleBracketsValidator < ActiveModel::Validator
   def validate(record)
-    stack  = []
+    stack = []
     lookup = { '(' => ')', '[' => ']', '{' => '}'}
-    left   = lookup.keys
-    right  = lookup.values
+    i = 0
+    left = lookup.keys
+    right = lookup.values
     record.title.each_char do |char|
       if left.include? char
+        i = 0
         stack << char
       elsif right.include? char
-        if stack.empty? || (lookup[stack.pop] != char)
-          record.errors.add(:base, "This record is invalid")
+
+        if stack.empty?
+          record.errors.add(:base, "Too many or missing opening brackets")
+        else
+          a = stack.pop
+          if lookup[a] != char
+              record.errors.add(:base, "Too many or incorrect closing brackets")
+              stack << a
+          elsif i == 0
+            record.errors.add(:base, "Empty brackets")
+          end
         end
+      else
+        i=i+1
       end
     end
-    return stack.empty?
+    if !stack.empty?
+      record.errors.add(:base, "Not all brackets are closed")
+    end
   end
 end
